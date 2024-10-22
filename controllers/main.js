571931d25cca80473e105fa541f154feb4fe91eb
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-10-22 13:40:43 +0200

    basic login JWT Auth
    added basic JWT auth

--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,21 @@
-
+const CustomAPIError = require("../errors/customError.js");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
-    res.send("Fake Login/Register/Signup Route");
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        throw new CustomAPIError("Please provide email and password!", 400);
+    };
+
+    // just for demo, since we are not connecting to DB yet.
+    const id = new Date().getDate();
+
+    // try to keep payload small, better experience for the user
+    // in production use, long and complex string values (JWT_SECRET)
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: "30d" });
+
+    res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
