54890819e2c7977880b78aa5fd65daff4047a8a4
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-11-06 13:02:12 +0100

    Status Codes
    added http-status-codes module. refactor the error handling accordingly.

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require("./customError.js");
+const BadRequestError = require("./badRequest.js");
+const UnauthenticatedError = require("./unauthenticated.js");
+
+module.exports = {
+    CustomAPIError,
+    BadRequestError,
+    UnauthenticatedError
+};
