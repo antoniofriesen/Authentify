0043485c220e0e6dc4a0f15623e2957ccfae0c23
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-11-06 12:51:40 +0100

    auth middleware
    added auth middleware to clean up the code

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { login, dashboard } = require("../controllers/main.js");
 
-router.route("/dashboard").get(dashboard);
+const authMiddleware = require("../middleware/auth.js");
+
+router.route("/dashboard").get(authMiddleware, dashboard);
 router.route("/login").post(login);
 
 module.exports = router;
